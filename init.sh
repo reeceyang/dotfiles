7fe068fcc5483566b9cfa65240f1d06229eecf68
reeceyang <reeceyang@live.com>
2021-01-06 21:40:12 -0600

    fix init.sh

--- init.sh
+++ init.sh
@@ -3,8 +3,8 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # zsh
-ln -s ${BASEDIR}/zshrc ~/.zshrc
-ln -s ${BASEDIR}/zshenv ~/.zshenv
+ln -s ${BASEDIR}/.zshrc ~/.zshrc
+ln -s ${BASEDIR}/.zshenv ~/.zshenv
 
 # git
-ln -s ${BASEDIR}/gitconfig ~/.gitconfig
+ln -s ${BASEDIR}/.gitconfig ~/.gitconfig
